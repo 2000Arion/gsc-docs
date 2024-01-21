d4f4ec858651232b07be32f752b79a678730e7df
Arion2000 <github@arion2000.xyz>
2024-01-21 18:24:26 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,9 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from 'prism-react-renderer';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
