e21fe403206b4195e5a6204b16f1756861156154
Arion2000 <github@arion2000.xyz>
2024-01-21 14:16:03 +0100

    ➕ Add Vercel SpeedInsights

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -29,7 +29,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -38,6 +38,7 @@ export default function Home() {
       <main>
         <HomepageFeatures />
       </main>
+      <SpeedInsights /> {/* SpeedInsights-Komponente von Vercel */}
     </Layout>
   );
 }
