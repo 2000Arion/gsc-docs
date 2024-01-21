9744ab7c3b9e1ce9147f88f52f82422874bbc708
Arion2000 <github@arion2000.xyz>
2024-01-21 13:34:09 +0100

    🆕 Styles hinzugefügt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -139,7 +139,7 @@ const config = {
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        darkTheme: prismThemes.oneDark,
       },
     }),
 };
