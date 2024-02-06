0b69b78f2bd72029f7a1247022bae64243f7ce98
Arion2000 <github@arion2000.xyz>
2024-02-06 15:34:55 +0100

    Add vercel speed insights

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -38,6 +38,7 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
       </main>
+      <SpeedInsights />
     </Layout>
   );
 }
