7150fd3ee25a0644963b6c8708a00d3f0d2e451c
Arion2000 <github@arion2000.xyz>
2024-08-07 13:24:45 +0200

    Add tawk.to chat widget

---
+++ config/scripts.config.ts
@@ -0,0 +1,22 @@
+const scripts = [
+  {
+    src: "https://embed.tawk.to/61e29fcdf7cf527e84d24447/1fpek0fbh",
+    async: true,
+    charset: "UTF-8",
+    crossorigin: "*",
+    type: 'text/javascript',
+    content: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/61e29fcdf7cf527e84d24447/1fpek0fbh';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+  },
+];
+
+export default scripts;
