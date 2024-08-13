7a6941bcb34eb1dc425976711e44684b54746ce5
Arion2000 <github@arion2000.xyz>
2024-08-13 22:18:41 +0200

    Copyright Hinweis Reihenfolge geändert

--- config/footer.config.ts
+++ config/footer.config.ts
@@ -88,7 +88,7 @@ const footer: Footer = {
       ],
     },
   ],
-  copyright: `<p><a class="footer__copyright_notice" rel="noopener nofollow noreferrer" href="https://www.arion2000.xyz" target="_blank"><img src="/assets/images/footer_copyright_watermark.png" alt="a2data logo"/></a> &copy; ${new Date().getFullYear()}</p><div id="footer__copyright_git">Build: <a href="https://github.com/2000Arion/gsc-docs/commit/${currentCommit}">v${packageVersion}/${currentCommit}</a> - ${commitDate}</div>`,
+  copyright: `<p>&copy; ${new Date().getFullYear()} <a class="footer__copyright_notice" rel="noopener nofollow noreferrer" href="https://www.arion2000.xyz" target="_blank"><img src="/assets/images/footer_copyright_watermark.png" alt="a2data logo"/></a></p><div id="footer__copyright_git">Build: <a href="https://github.com/2000Arion/gsc-docs/commit/${currentCommit}">v${packageVersion}/${currentCommit}</a> - ${commitDate}</div>`,
 };
 
 export default footer;
